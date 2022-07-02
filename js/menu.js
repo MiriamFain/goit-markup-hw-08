9f6fd19a9a38dc4b6c41573d709af4e0e1f82a48
Miriam Fainshtein <104686417+MiriamFain@users.noreply.github.com>
2022-07-02 22:24:27 +0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,33 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector("[data-menu-open]"),
+//     closeMenuBtn: document.querySelector("[data-menu-close]"),
+//     menu: document.querySelector("[data-menu]"),
+//     body: document.querySelector("body"),
+//   };
+//   console.log(refs);
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//     refs.body.classList.toggle("no-scroll");
+//   }
+// })();
+
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector(".menu-open-btn"),
-    closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".mob-menu"),
-    body: document.querySelector("body"),
+    body: document.body,
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
   };
-
+  console.log(refs);
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
     refs.body.classList.toggle("no-scroll");
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
